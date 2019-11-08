16dddee34c67bcf831842635b169e743089af29a
Vukasin Stancevic <stihilus@gmail.com>
2019-11-08 21:03:41 +0100

    new experiments added

---
+++ Typewriter Drawings/preload.js
@@ -0,0 +1,24 @@
+// Loading all images from /assets folder
+function preload() {
+  bgd01 = loadImage("bgd01.jpg");
+  frame = loadImage("frame.png");
+  
+  img01 = loadImage("assets/img01.png");
+  img02 = loadImage("assets/img02.png");
+  img03 = loadImage("assets/img03.png");
+  img04 = loadImage("assets/img04.png");
+  img05 = loadImage("assets/img05.png");
+  img06 = loadImage("assets/img06.png");
+  img07 = loadImage("assets/img07.png");
+  img08 = loadImage("assets/img08.png");
+  img09 = loadImage("assets/img09.png");
+  img10 = loadImage("assets/img10.png");
+  img11 = loadImage("assets/img11.png");
+  img12 = loadImage("assets/img12.png");
+  img13 = loadImage("assets/img13.png");
+  img14 = loadImage("assets/img14.png");
+  img15 = loadImage("assets/img15.png");
+  img16 = loadImage("assets/img16.png");
+  img17 = loadImage("assets/img17.png");
+  img18 = loadImage("assets/img18.png");
+}
